ce13f3e5951f030e9fa3e4024e6b5d1d803cb005
Rodrigo <lucianorodrigocarvalho@gmail.com>
2024-11-20 12:42:23 -0300

    Create project

---
+++ src/main/java/com/smartprocess/projCalcRendSucata/ProjCalcRendSucataApplication.java
@@ -0,0 +1,13 @@
+package com.smartprocess.projCalcRendSucata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjCalcRendSucataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjCalcRendSucataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartprocess/projCalcRendSucata/ProjCalcRendSucataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartprocess.projCalcRendSucata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjCalcRendSucataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
